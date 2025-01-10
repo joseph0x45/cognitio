4934c7ee129a26db48a789ea1617c2f03acf0494
DarylKue <darylkue@gmil.com>
2025-01-10 21:48:18 +0000

    Adding first layout of cognitio admin login page

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -3,5 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("test", "routes/test.tsx"),
-  route("bruh", "routes/bruh.tsx")
+  route("bruh", "routes/bruh.tsx"),
+  //just a sample
+  route("admin/login", "pages/adminLogin.tsx")
 ] satisfies RouteConfig;
